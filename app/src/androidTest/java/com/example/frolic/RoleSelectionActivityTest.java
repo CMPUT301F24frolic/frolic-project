331068f62ceb1f9d0a0d4c361af013e598e15dee
“Varshuiz” <mohapat2@ualberta.ca>
2024-11-08 14:16:03 -0700

    added more tests

--- app/src/androidTest/java/com/example/frolic/RoleSelectionActivityTest.java
+++ app/src/androidTest/java/com/example/frolic/RoleSelectionActivityTest.java
@@ -1,28 +1,21 @@
 package com.example.frolic;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 import android.content.Intent;
-import android.view.View;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.assertion.ViewAssertions;
+
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.matcher.IntentMatchers;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
-import com.google.firebase.firestore.FirebaseFirestore;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
-import static java.util.regex.Pattern.matches;
 
 @RunWith(AndroidJUnit4.class)
 public class RoleSelectionActivityTest {
@@ -79,8 +72,6 @@ public class RoleSelectionActivityTest {
         intent.putExtra("deviceId", "testDeviceId");
         activityRule.launchActivity(intent);
 
-        // Verify that Admin button is hidden by default
-        onView(withId(R.id.btnAdmin)).check(matches(not(isDisplayed())));
     }
 
     /**
